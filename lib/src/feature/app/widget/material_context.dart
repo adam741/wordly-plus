23cfe0ba45b0e430395126c9994d96088f246e0d
adam741 <adam49863@gmail.com>
2026-09-14 09:15:52 +0300

    Update material_context.dart

--- lib/src/feature/app/widget/material_context.dart
+++ lib/src/feature/app/widget/material_context.dart
@@ -55,7 +55,6 @@ class _MaterialContextState extends State<MaterialContext> {
           localizationsDelegates: Localization.localizationDelegates,
           supportedLocales: Localization.supportedLocales,
           locale: locale,
-          onGenerateTitle: (context) => context.l10n.appTitle,
           debugShowCheckedModeBanner: false,
           home: const GamePage(),
           builder: (context, child) => KeyedSubtree(
