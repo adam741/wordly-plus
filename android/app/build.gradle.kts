b11864894b89996e3fef8a6ef02544af7788d884
adam741 <adam49863@gmail.com>
2026-09-14 04:27:17 +0300

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 android {
-  namespace = "com.wordianguess.app"
+  namespace = "com.app.wordsixquest"
   compileSdk = 37
   ndkVersion = "29.0.14206865"
 
@@ -21,7 +21,7 @@ android {
   }
 
   defaultConfig {
-    applicationId = "com.wordianguess.app"
+    applicationId = "com.app.wordsixquest"
     minSdk = 24
     targetSdk = 37
     versionCode = flutter.versionCode
