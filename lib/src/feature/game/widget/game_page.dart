a75c969f513151b3880567651feb726aa468912c
adam741 <adam49863@gmail.com>
2026-09-15 20:56:48 +0300

    Update game_page.dart

--- lib/src/feature/game/widget/game_page.dart
+++ lib/src/feature/game/widget/game_page.dart
@@ -137,7 +137,7 @@ class const GameBody({super.key}) extends StatelessWidget {
                 previous.dictionary == current.dictionary &&
                 current.isResult) ||
             current.isFailure,
-        listener: (context, state) {
+        listener: (context, state) async {
           if (state.isResult) {
             ScaffoldMessenger.of(context).hideCurrentSnackBar();
             final GameBloc bloc = context.read<GameBloc>();
